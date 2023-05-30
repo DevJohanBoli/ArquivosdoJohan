ca44785229c4ca8af63baea97f77103fcfe0051b
DevJohanBoli <129955638+DevJohanBoli@users.noreply.github.com>
2023-05-30 11:58:48 -0300

    Add files via upload

---
+++ Aulas de Javascript/14º function/script.js
@@ -0,0 +1,19 @@
+//funções são usadas em programação para executar ações que são rotineiramente em um programa
+function soma(a,b){
+    return a + b
+}
+// vai no console e digita"soma (2(a),2(b))" 
+let x = soma(5,6)
+console.log(x)
+
+//multiplicação
+
+function multiplicar(c,d,e){
+    return c*d*e
+}
+
+//Arrow Funcion
+
+let substração = () =>{
+    return a-b
+}
---
+++ Aulas de Javascript/exercicio/script.js
@@ -0,0 +1,34 @@
+//crie função para identificar se o numero é par ou impar
+//ParouImpar(5)
+//ParouImpar(4)
+let impar=(x) =>{
+    if(x % 2 == 0){
+        return console.log("o numero é par")
+    }
+    else{
+        return console.log("o numero é impar")
+    }
+}
+
+//
+
+function ParOuImpar(a){
+    if(a % 2 == 0){
+        return console.log("par")
+    }
+    else{
+        return console.log("impar")
+    }
+}
+
+//
+
+let impar = (b)=>{
+    if(b % 2 == 0){
+        return console.log("par")
+    }
+    else{
+        return console.log("impar")
+    }
+}
+}
